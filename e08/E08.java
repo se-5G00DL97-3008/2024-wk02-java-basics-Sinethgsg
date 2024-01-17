8cbec808bcf585728e29c34034b4b5fc93bdb92d
Sineth Gayashan <sineth.gallage@tuni.fi>
2024-01-18 00:47:14 +0200

    exercises added

--- e08/E08.java
+++ e08/E08.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 class E08 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean valid = true;
+        while (valid) {
+            System.out.println("Give a number (0 to quit):");
+            int input = scanner.nextInt();
+
+            if (input == 0) {
+                valid = false;
+            } else {
+                if (input % 2 == 0) {
+                    System.out.println("Number is even");
+                } else {
+                    System.out.println("Number is odd");
+                }
+            }
+        }
 
     }
 }
